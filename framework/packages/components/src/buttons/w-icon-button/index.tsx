1b29f3502b65834b36251f12478c28e6d6c8a877
MEHMET BARAN (OUTSOURCE) <mehmetbaran@mehmetbaran.net>
2018-07-26 13:32:56 +0300

    WIconButton'a icon propertysi eklendi

--- framework/packages/components/src/buttons/w-icon-button/index.tsx
+++ framework/packages/components/src/buttons/w-icon-button/index.tsx
@@ -1,11 +1,19 @@
 import * as React from 'react';
 import { IconButton, Icon } from '@material-ui/core';
 import { IconButtonProps } from '@material-ui/core/IconButton';
+import { WIcon } from '../../w-icon'
 
-export interface WIconButtonProps extends IconButtonProps {}
+export interface WIconButtonProps extends IconButtonProps {
+    icon?: string
+}
 
 export class WIconButton extends React.Component<WIconButtonProps, {}> {
-    public render() {
-        return <IconButton {...this.props} />
+    public render() {        
+        return (
+            <IconButton {...this.props}>{
+                  this.props.icon ? <WIcon>this.props.icon</WIcon> : this.props.children
+                }
+            </IconButton>
+        );
     }
 }
