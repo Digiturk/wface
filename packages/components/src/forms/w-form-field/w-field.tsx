538974ae6327b67b78a2be00ce196eda91bc2571
MEHMET BARAN (IT) <mehmetbaran@mehmetbaran.net>
2018-11-15 11:34:32 +0300

    WFormField.TextField dialoglarda ilk karakterde focus kaybı problemi giderildi

--- packages/components/src/forms/w-form-field/w-field.tsx
+++ packages/components/src/forms/w-form-field/w-field.tsx
@@ -2,15 +2,15 @@ import * as React from 'react';
 import { Field, FastField } from 'formik';
 
 const WField = props => {
-  const component = subProps => ( 
-    <div style={{marginTop: 5, marginBottom: 5}}>
-      <props.component {...subProps}/>
+  const Component = subProps => ( 
+    <div style={{marginTop: 5, marginBottom: 5}} {...subProps}>
+      {props.component(subProps)}
     </div>
   )
   return (
     <FastField 
       {...props} 
-      component={component}
+      render={Component}
     />
   );
 }
--- packages/wface/src/screens/DemoScreen.tsx
+++ packages/wface/src/screens/DemoScreen.tsx
@@ -3,6 +3,7 @@ import * as WFace from '@wface/components';
 import { WFormValidation } from '@wface/components';
 
 interface DemoScreenState {
+  isDialogOpen: boolean;
   userData: any,
   lookup: any,
   textValue: string,
@@ -16,51 +17,29 @@ export class DemoScreen extends React.Component<WFace.BaseScreenProps, DemoScree
 
     this.state = this.props.screenData.state || {
       userData: {},
+      isDialogOpen: false,
       lookup: [
-        { label: 'Gaziantep', value: 27 },
+        { label: 'Gaziantep', value: '27' },
         { label: 'İstanbul', value: '34' },
-        { label: 'İzmir', value: 35 },
-        { label: 'Şanlıurfa', value: 63 },
+        { label: 'İzmir', value: '35' },
+        { label: 'Şanlıurfa', value: '63' },
       ],
       selectValue: 35,
       selectObject: {}
     }
   }
 
-  public render() {    
+  public render() {
     return (
-      <WFace.WCard>
-        <WFace.WTable
-          columns={[
-            { title: 'Name', field: 'name' },
-            { title: 'Surname', field: 'surname' },
-            { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
-            {
-              title: 'Birth Place',
-              field: 'birthCity',
-              lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
-            },
-          ]}
-          data={[
-            { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
-            { name: 'Zerya Betül', surname: 'Baran', birthYear: 2017, birthCity: 34 },
-          ]}
-          title="Selection Change Event"
-          actions={[
-            {
-              icon: 'done_all',
-              tooltip: 'Do',
-              onClick: (event, rows) => {
-                alert('You selected ' + rows.length + ' rows')
-              },
-            },
-          ]}
-          options={{
-            selection: true,
-          }}
-          onSelectionChange={(data:[]) => alert('you selected ' + data.length + ' rows')}
-        />
-      </WFace.WCard>
+      <React.Fragment>
+        <WFace.WButton onClick={() => this.setState({ isDialogOpen: true })}>Aç</WFace.WButton>
+        <WFace.WDialog open={this.state.isDialogOpen} onClose={() => this.setState({ isDialogOpen: false })}>
+
+          <WFace.WDialogContent>
+            {this.renderForm()}
+          </WFace.WDialogContent>
+        </WFace.WDialog>
+      </React.Fragment>
     )
   }
 
@@ -93,7 +72,7 @@ export class DemoScreen extends React.Component<WFace.BaseScreenProps, DemoScree
                   <WFace.WFormField.Select name="select" label="Form Select" options={this.state.lookup} />
                   <WFace.WFormField.Select name="selectMulti" label="Form Select Multi" options={this.state.lookup} isMulti />
                   <WFace.WFormField.Switch name="switch" label="Form Switch" />
-                  <WFace.WFormField.TextField name="text" label="Form TextField" />
+                  <WFace.WFormField.TextField name="text" label="Form TextField" id="text"/>
                   <WFace.WFormField.TimePicker name="time" label="Form Time" />
                 </WFace.WCardContent>
                 <WFace.WCardActions>
