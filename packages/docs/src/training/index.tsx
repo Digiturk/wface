54b9d35b4780ba9ad450156e2df3b55dd34811f5
MEHMET BARAN (TECHNOLOGY) <mehmetbaran@mehmetbaran.net>
2018-11-23 16:44:47 +0300

    slide

--- packages/docs/src/training/index.tsx
+++ packages/docs/src/training/index.tsx
@@ -1,13 +1,15 @@
 import * as React from 'react';
 import * as WFace from '@wface/components';
-import { Entry } from './slides/1-entry';
-import { Web } from './slides/2-web';
-import { Browser } from './slides/3-browser';
-import { Dom } from './slides/4-dom';
-import { DevTools} from './slides/5-dev-tools'
-import { HtmlSlide} from './slides/6-html'
-import { HtmlCssSlide } from './slides/8-html-css';
-import { CssSlide } from './slides/7-css';
+import { Entry } from './slides/01-entry';
+import { Web } from './slides/02-web';
+import { Browser } from './slides/03-browser';
+import { Dom } from './slides/04-dom';
+import { DevTools} from './slides/05-dev-tools'
+import { HtmlSlide} from './slides/06-html'
+import { CssSlide } from './slides/07-css';
+import { HtmlCssSlide } from './slides/08-html-css';
+import { JavaScript } from './slides/09-javascript';
+import { JavaScriptCode } from './slides/10-javascript-code';
 
 export default class TrainingPage extends React.Component<any, any> {
   slides = [
@@ -18,13 +20,15 @@ export default class TrainingPage extends React.Component<any, any> {
     { component: DevTools, header: 'DevTools' },
     { component: HtmlSlide, header: 'HTML' },
     { component: CssSlide, header: 'CSS' },
-    { component: HtmlCssSlide, header: 'HTML & CSS Example' },
+    { component: HtmlCssSlide, header: 'HTML & CSS Code' },
+    { component: JavaScript, header: 'JavaScript' },
+    { component: JavaScriptCode, header: 'JavaScript Code' },
   ];
 
   constructor(props: any) {
     super(props);
     this.state = {
-      activeSlide: 6
+      activeSlide: 0
     }
   }
 
---
+++ packages/docs/src/training/slides/03-browser/components/dns.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+
+export interface DnsProps {
+}
+
+export class Dns extends React.Component<DnsProps, any> {
+  colors = {
+    client: '#3F51B5',
+    network: '#FFA726',
+    server: '#4CAF50'
+  }
+
+  public render() {
+    return (
+      <div style={{display: 'table', height: '100%', width: '100%'}}>
+        <div style={{display: 'table-cell', verticalAlign: 'middle', transform: 'scale(1.2)'}}>
+          <img src="/assets/slide/dns.png"/>
+        </div>
+      </div >
+    );
+  }
+}
+
+
