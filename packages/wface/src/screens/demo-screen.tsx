2e86beec73567e38fcbab873381eb4d2af8cb637
MEHMET BARAN (TECHNOLOGY) <mehmetbaran@mehmetbaran.net>
2018-12-31 11:10:27 +0300

    wface-97 WTable secili kayitlarin kaybolmasi problemi giderildi

--- packages/wface/src/screens/demo-screen.tsx
+++ packages/wface/src/screens/demo-screen.tsx
@@ -2,8 +2,6 @@ import * as React from 'react';
 import * as WFace from '@wface/components';
 
 interface DemoScreenState {
-  isLoading: boolean;
-  text: string
 }
 
 export class DemoScreen extends React.Component<WFace.BaseScreenProps, DemoScreenState> {
@@ -11,61 +9,34 @@ export class DemoScreen extends React.Component<WFace.BaseScreenProps, DemoScree
     super(props);
 
     this.state = this.props.screenData.state || {
-      isLoading: false,
-      text: 'A'
     }
   }
 
   public render() {
-    
-    return (
-      <>        
-        Text: {this.state.text}
-        <br/>
-        <ImageUpload 
-          src="https://www.w3schools.com/howto/img_avatar.png" 
-          isLoading={this.state.isLoading}
-          size={128}
-          onChange={(e) => console.log(e.target.files)}
-        />        
 
-        <WFace.WButton onClick={() => {         
-          this.setState({isLoading: true, text: 'B'}, () => {
-            this.props.changeScreenMode('loading');
-            setTimeout(() => { 
-              this.setState({isLoading: false, text: 'C'}, () => this.props.changeScreenMode('normal'));
-            }, 3000)
-          });
-          
-        }}>
-          Getir
-        </WFace.WButton>
-        
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <WFace.WButton onClick={() => this.setState({isLoading: !this.state.isLoading})}>Load</WFace.WButton>
+    return (
+      <>
+        This is demo screen
       </>
     )
   }
 }
 
-const ImageUpload = props => {  
+const ImageUpload = props => {
   const size = props.size || 48;
   return (
     <label style={{ display: 'inline-flex', position: 'relative', width: size, height: size }}>
       <WFace.WAvatar
         sizes={size}
-        style={{width: size, height:size, display: 'inline-block'}}
+        style={{ width: size, height: size, display: 'inline-block' }}
         src={props.src}
       />
       {props.isLoading &&
         <div style={{ position: 'absolute', width: size, height: size, background: '#FFFFFF88', borderRadius: '50%' }}>
-          <WFace.WCircularProgress size={size}/>
+          <WFace.WCircularProgress size={size} />
         </div>
       }
-      
+
       {/* <div style={{ position: 'absolute', padding: 4, backgroundColor: 'white', right: 7, bottom: 7, borderRadius: '50%', color: 'white' }}>
         <WFace.WIcon icon="camera" />
       </div>
