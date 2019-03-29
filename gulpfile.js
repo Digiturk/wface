2d4796f200f94db64fd547c41968167dcbdfc05d
mbrn <mehmetbaran@mehmetbaran.net>
2019-03-29 11:13:53 +0300

    version upgrade to 0.16.0

--- gulpfile.js
+++ gulpfile.js
@@ -6,19 +6,19 @@ var minimist = require('minimist');
 var knownOptions = { string: 'version' };
 var options = minimist(process.argv.slice(2), knownOptions);
 
-gulp.task('publish', function() {
+gulp.task('publish', function () {
   const command = "npm publish --access=public"
 
-  childProcess.execSync(command, { stdio: [0,1,2], cwd: './packages/components/' });
-  childProcess.execSync(command, { stdio: [0,1,2], cwd: './packages/container/' });
-  childProcess.execSync(command, { stdio: [0,1,2], cwd: './packages/ioc/' });
-  childProcess.execSync(command, { stdio: [0,1,2], cwd: './packages/store/' });
-  childProcess.execSync(command, { stdio: [0,1,2], cwd: './packages/cli/' });
+  childProcess.execSync(command, { stdio: [0, 1, 2], cwd: './packages/components/' });
+  childProcess.execSync(command, { stdio: [0, 1, 2], cwd: './packages/container/' });
+  childProcess.execSync(command, { stdio: [0, 1, 2], cwd: './packages/ioc/' });
+  childProcess.execSync(command, { stdio: [0, 1, 2], cwd: './packages/store/' });
+  childProcess.execSync(command, { stdio: [0, 1, 2], cwd: './packages/cli/' });
 });
 
-gulp.task('version', function(){  
+gulp.task('version', function () {
   const newVersion = options.version;
-  if(!newVersion) {
+  if (!newVersion) {
     console.error('Please specify version parameter!');
     return;
   }
@@ -29,16 +29,28 @@ gulp.task('version', function(){
   updateVersion('./packages/cli/package.json', newVersion);
 });
 
+gulp.task('versions', function () {
+  const print = (project) => {
+    console.log(project + ": " + require('./packages/' + project + '/package.json').version);
+  }
+
+  print('components');
+  print('container');
+  print('ioc');
+  print('store');
+  print('cli');
+});
+
 const updateVersion = (path, version) => {
   const package = require(path);
   console.log(package.name + " old version is " + package.version);
 
   package.version = version;
-  if(package.dependencies["@wface/components"]) package.dependencies["@wface/components"] = version;
-  if(package.dependencies["@wface/container"]) package.dependencies["@wface/container"] = version;
-  if(package.dependencies["@wface/ioc"]) package.dependencies["@wface/ioc"] = version;
-  if(package.dependencies["@wface/store"]) package.dependencies["@wface/store"] = version;
+  if (package.dependencies["@wface/components"]) package.dependencies["@wface/components"] = version;
+  if (package.dependencies["@wface/container"]) package.dependencies["@wface/container"] = version;
+  if (package.dependencies["@wface/ioc"]) package.dependencies["@wface/ioc"] = version;
+  if (package.dependencies["@wface/store"]) package.dependencies["@wface/store"] = version;
 
   const data = JSON.stringify(package, null, "\t");
-  fs.writeFileSync(path, data);  
+  fs.writeFileSync(path, data);
 }
