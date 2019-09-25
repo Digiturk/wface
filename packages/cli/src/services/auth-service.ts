142c64c17733159b467b6e0927ccea5516f6d891
mbrn <mehmetbaran@mehmetbaran.net>
2019-09-25 08:25:13 +0300

    navigation problems resolved

--- packages/cli/src/services/auth-service.ts
+++ packages/cli/src/services/auth-service.ts
@@ -5,7 +5,7 @@ import { UserContext } from '@wface/store';
 @injectable()
 export default class AuthService implements IAuthService {
 
-  @inject("UserContext") userContext : UserContext;
+  @inject("UserContext") userContext: UserContext;
 
   public login(username: string, password: string, values?: any): Promise<{ displayName: string, token?: string }> {
     return new Promise((resolve, reject) => {
@@ -17,25 +17,36 @@ export default class AuthService implements IAuthService {
         setTimeout(() => reject("Wrong username or password"), 1000);
       }
 
-      setTimeout(() => resolve({displayName: 'MockUser', token: 'MockToken'}), 1500);
+      setTimeout(() => resolve({ displayName: 'MockUser', token: 'MockToken' }), 1500);
     });
   }
 
   public getMenuTree(): Promise<IMenuTreeItem[]> {
 
     return new Promise((resolve, reject) => {
-      const result:IMenuTreeItem[] = [];
+      const result: IMenuTreeItem[] = [];
       let id = 0;
       result.push({
         id: (++id).toString(),
         text: 'DemoScreen1',
-        screen: 'DemoScreen', 
+        screen: 'DemoScreen',
         icon: 'save'
-      });    
+      });
       result.push({
         id: (++id).toString(),
         text: 'DemoScreen2',
         screen: 'DemoScreen2'
+      });
+      result.push({
+        id: (++id).toString(),
+        text: 'menu',
+        subNodes: [
+          {
+            id: (++id).toString(),
+            text: 'sub screen',
+            screen: 'DemoScreen2'
+          }
+        ]
       });
       // for(let i = 3; i < 20; i++) {
       //   result.push({
