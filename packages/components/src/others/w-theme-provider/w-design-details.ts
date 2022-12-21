4eea41c49f29e88994fc3b484e5d4daf18bf8dc9
dtaksan <116544227+dtaksan@users.noreply.github.com>
2022-12-21 15:08:46 +0300

    add-drawer-customize-style

--- packages/components/src/others/w-theme-provider/w-design-details.ts
+++ packages/components/src/others/w-theme-provider/w-design-details.ts
@@ -1,4 +1,11 @@
+import { DrawerMenuItemProps } from "../../../../container/src/components/w-main-page/nav-list";
+import { SxProps } from '@mui/material';
+
 export interface WDesignDetails {
   defaultElevation?: number;
   pagePadding?: number | string;
+  drawerDesign?: {
+    paper?: SxProps;
+    menuItem?: DrawerMenuItemProps;
+  };
 }
