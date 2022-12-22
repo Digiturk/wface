849584cf74b36d7415814f59d32be8a6b2d404fe
DTCYAMAN <ceren.yaman@ext.digiturk.com.tr>
2022-12-22 11:24:59 +0300

    added WChip for ıssue  #70

--- packages/container/src/components/w-main-page/my-profile-menu.tsx
+++ packages/container/src/components/w-main-page/my-profile-menu.tsx
@@ -1,16 +1,30 @@
-import { WIcon, WIconButton, WListItemIcon, WListItemText, WMenu, WMenuItem } from '@wface/components';
-import { UserContextActions, WStore, AppContextActions } from '@wface/store';
-import * as React from 'react';
-import { connect } from 'react-redux';
-import IOC, { IMenuTreeItem, MenuTreeUtil } from '@wface/ioc';
-import IAppHooks from '@wface/ioc/src/interfaces/i-app-hooks';
+import {
+  WAvatar,
+  WChip,
+  WIcon,
+  WIconButton,
+  WListItemIcon,
+  WListItemText,
+  WMenu,
+  WMenuItem
+} from "@wface/components";
+import { UserContextActions, WStore, AppContextActions } from "@wface/store";
+import * as React from "react";
+import { connect } from "react-redux";
+import IOC, { IMenuTreeItem, MenuTreeUtil } from "@wface/ioc";
+import IAppHooks from "@wface/ioc/src/interfaces/i-app-hooks";
 
 interface MyProfileMenuState {
-  userMenuAnchor?: HTMLElement
+  userMenuAnchor?: HTMLElement;
 }
 
 export interface MyProfileMenuProps {
-  items: { id: string, icon?: string, text: string, onClick?: ((event: any) => void) | string }[];
+  items: {
+    id: string;
+    icon?: string;
+    text: string;
+    onClick?: ((event: any) => void) | string;
+  }[];
 }
 
 export interface DispatchProps {
@@ -19,15 +33,17 @@ export interface DispatchProps {
   logout: () => void;
 }
 
-class MyProfileMenu extends React.Component<MyProfileMenuProps & WStore & DispatchProps, MyProfileMenuState> {
-
+class MyProfileMenu extends React.Component<
+  MyProfileMenuProps & WStore & DispatchProps,
+  MyProfileMenuState
+> {
   constructor(props) {
     super(props);
     this.state = {
       userMenuAnchor: null
-    }
+    };
 
-    this.logoutClicked = this.logoutClicked.bind(this)
+    this.logoutClicked = this.logoutClicked.bind(this);
   }
 
   logoutClicked() {
@@ -38,8 +54,7 @@ class MyProfileMenu extends React.Component<MyProfileMenuProps & WStore & Dispat
         const hooks = IOC.get<IAppHooks>("IAppHooks");
         hooks.onLogout && hooks.onLogout();
       }
-    }
-    catch (e) {
+    } catch (e) {
       console.log(e);
     }
 
@@ -51,39 +66,50 @@ class MyProfileMenu extends React.Component<MyProfileMenuProps & WStore & Dispat
       <div>
         <WIconButton
           id="btn-main-more"
-          aria-owns={Boolean(this.state.userMenuAnchor) ? 'menu-appbar' : null}
+          aria-owns={Boolean(this.state.userMenuAnchor) ? "menu-appbar" : null}
           aria-haspopup="true"
-          onClick={(event) => this.setState({ userMenuAnchor: event.currentTarget })}
+          onClick={(event) =>
+            this.setState({ userMenuAnchor: event.currentTarget })
+          }
           color="inherit"
         >
-          <WIcon>more_vert</WIcon>
+          <WChip
+            avatar={
+              <WAvatar style={{ width: "40px", height: "40px" }}></WAvatar>
+            }
+            label=""
+            clickable
+            color="primary"
+          />
         </WIconButton>
         <WMenu
           id="menu-appbar"
           anchorEl={this.state.userMenuAnchor}
           anchorOrigin={{
-            vertical: 'top',
-            horizontal: 'right',
+            vertical: "top",
+            horizontal: "right"
           }}
           transformOrigin={{
-            vertical: 'top',
-            horizontal: 'right',
+            vertical: "top",
+            horizontal: "right"
           }}
           open={Boolean(this.state.userMenuAnchor)}
           onClose={() => this.setState({ userMenuAnchor: null })}
         >
           {this.props.items &&
-            this.props.items.map(item => (
+            this.props.items.map((item) => (
               <WMenuItem
                 id={item.id}
                 key={item.id}
                 onClick={(e) => {
-                  if (typeof item.onClick === 'function') {
+                  if (typeof item.onClick === "function") {
                     item.onClick(e);
                     this.setState({ userMenuAnchor: null });
-                  }
-                  else {
-                    const screenItem = MenuTreeUtil.findByName(this.props.appContext.menuTree, item.onClick);
+                  } else {
+                    const screenItem = MenuTreeUtil.findByName(
+                      this.props.appContext.menuTree,
+                      item.onClick
+                    );
                     if (!screenItem) {
                       return;
                     }
@@ -91,35 +117,43 @@ class MyProfileMenu extends React.Component<MyProfileMenuProps & WStore & Dispat
                     this.props.openScreen(screenItem);
                     this.setState({ userMenuAnchor: null });
                   }
-                }}>
-                {item.icon &&
-                  <WListItemIcon style={{minWidth: 'auto'}}>
+                }}
+              >
+                {item.icon && (
+                  <WListItemIcon style={{ minWidth: "auto" }}>
                     <WIcon>{item.icon}</WIcon>
                   </WListItemIcon>
-                }
-                <WListItemText style={{marginLeft: 12}} primary={item.text} />
+                )}
+                <WListItemText style={{ marginLeft: 12 }} primary={item.text} />
               </WMenuItem>
-            ))
-          }
-          {this.props.appContext.configuration.authRequired && 
-            <WMenuItem id="menu-item-logout" key="menu-item-logout" onClick={this.logoutClicked}>
-              <WListItemIcon style={{minWidth: 'auto'}}>
+            ))}
+          {this.props.appContext.configuration.authRequired && (
+            <WMenuItem
+              id="menu-item-logout"
+              key="menu-item-logout"
+              onClick={this.logoutClicked}
+            >
+              <WListItemIcon style={{ minWidth: "auto" }}>
                 <WIcon>exit_to_app</WIcon>
-              </WListItemIcon >
-              <WListItemText style={{marginLeft: 12}} primary="Çıkış" />
+              </WListItemIcon>
+              <WListItemText style={{ marginLeft: 12 }} primary="Çıkış" />
             </WMenuItem>
-          }
+          )}
         </WMenu>
       </div>
-    )
+    );
   }
 }
 
-const mapStateToProps = state => ({ ...state });
-const mapDispatchToProps = dispatch => ({
-  openScreen: (menuTreeItem: IMenuTreeItem, initialValues?: any) => dispatch(AppContextActions.openScreen({ menuTreeItem, initialValues })),
+const mapStateToProps = (state) => ({ ...state });
+const mapDispatchToProps = (dispatch) => ({
+  openScreen: (menuTreeItem: IMenuTreeItem, initialValues?: any) =>
+    dispatch(AppContextActions.openScreen({ menuTreeItem, initialValues })),
   clearAppContext: () => dispatch(AppContextActions.clear()),
   logout: () => dispatch(UserContextActions.logout())
-})
+});
 
-export default connect<WStore, DispatchProps, MyProfileMenuProps>(mapStateToProps, mapDispatchToProps)(MyProfileMenu as any);
+export default connect<WStore, DispatchProps, MyProfileMenuProps>(
+  mapStateToProps,
+  mapDispatchToProps
+)(MyProfileMenu as any);
