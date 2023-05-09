6169bdb657cbc49cef771f76c932f475a0f99599
dtaksan <116544227+dtaksan@users.noreply.github.com>
2023-05-09 15:44:06 +0300

    toolbar fixes b

--- demo/index.tsx
+++ demo/index.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useCallback, useEffect } from "react";
 import ReactDOM from "react-dom";
-import * as Screens from './src/screens';
-import { IConfiguration, useAppContext, useUserContext, WFace, WIcon, WTheme } from 'wface';
-import AuthService from '././src/services/auth-service';
+import * as Screens from "./src/screens";
+import {
+  IConfiguration,
+  useAppContext,
+  useUserContext,
+  WFace,
+  WButton,
+  WTheme,
+  WBox,
+} from "wface";
+import AuthService from "././src/services/auth-service";
+
+const Toolbar = () => {
+  return <h1 style={{ color: "white" }}>o2k3</h1>;
+};
 
 const configuration: IConfiguration = {
-  projectName: 'WFace Demo',
+  projectName: "WFace Demo",
   screenList: {
     DemoScreen: Screens.DemoScreen,
     DemoScreen2: Screens.DemoScreen2,
@@ -14,7 +26,7 @@ const configuration: IConfiguration = {
     DemoScreen5: Screens.DemoScreen5,
     EmptyScreen: Screens.EmptyScreen,
     SettingsScreen: Screens.SettingsScreen,
-    ContentDefinitionScreen: () => <div>ContentDefinitionScreen</div>
+    ContentDefinitionScreen: () => <div>ContentDefinitionScreen</div>,
   },
   useAuthService: AuthService,
   useLocalStorage: true,
@@ -22,15 +34,15 @@ const configuration: IConfiguration = {
     designDetails: {
       pageSx: {
         padding: (theme: WTheme) => theme.spacing(1),
-      }
-    }
+      },
+    },
   },
   api: {
-    baseUrl: 'http://localhost:8080',
+    baseUrl: "http://localhost:8080",
     useToken: () => {
       const userContext = useUserContext();
       return userContext.token;
-    }
+    },
   },
   search: true,
   useRightContextItems: () => {
@@ -38,21 +50,56 @@ const configuration: IConfiguration = {
 
     return [
       {
-        id: 'id1',
-        text: 'Ayarlar',
-        onClick: () => appContext.openScreenById('settings')
-      }
+        id: "id1",
+        text: "Ayarlar",
+        onClick: () => appContext.openScreenById("settings"),
+      },
     ];
   },
   components: {
-    TopbarRightItems: () => (
-      <WIcon sx={{ ml: 1 }}>notifications</WIcon>
-    )
-  }
-}
+    Toolbar: <Toolbar />,
+    ToolbarDrawerIcons: {
+      Menu: "home",
+      Close: "error",
+    },
+    TopbarRightItems: () => {
+      const appContext = useAppContext();
+
+      const changeMode = useCallback(
+        (mode: "CRM" | "CMS") => {
+          appContext.setValue("screenMode", mode);
+        },
+        [appContext]
+      );
+
+      return (
+        <WBox display={"flex"}>
+          <WButton
+            variant="contained"
+            color="secondary"
+            onClick={() => {
+              changeMode("CRM");
+            }}
+          >
+            CRM
+          </WButton>
+          <WButton
+            variant="contained"
+            color="secondary"
+            onClick={() => {
+              changeMode("CMS");
+            }}
+          >
+            CMS
+          </WButton>
+        </WBox>
+      );
+    },
+  },
+};
 
 const App = () => {
   return <WFace configuration={configuration} />;
-}
+};
 
 ReactDOM.render(<App />, document.getElementById("root"));
