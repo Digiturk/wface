c0d8c4c8acc2b97f2930c04442c40ef735d3f9bd
Mehmet Baran <mehmetbaran@mehmetbaran.net>
2023-05-25 22:48:26 +0300

    date picker fix

--- demo/src/screens/empty-screen/index.tsx
+++ demo/src/screens/empty-screen/index.tsx
@@ -1,28 +1,38 @@
-import React, { FC, useCallback, useState } from 'react';
-import { WDatePicker } from 'wface';
-import makeStyles from "@mui/styles/makeStyles";
+import { Alert } from '@mui/material';
+import React, { FC, useState } from 'react';
+import { WBox, WCard, WCardContent, WCardHeader, WDatePicker, WGrid, WTypography } from 'wface';
 
-const useStyles = makeStyles((theme: any) => ({
-  selectRoot: {
-    '& .w_select__input-container': {
-      background: 'white'
-    }
-  }
-}));
+const DateCard = (props: { title: string, children: React.ReactNode, value: Date }) => (
+  <WGrid item xs={12} sm={6} md={4} lg={3}>
+    <WCard>
+      <WCardHeader title={props.title} />
+      <WCardContent>
+        {props.children}
+        <WBox mt={1}>
+          Value: <WTypography fontWeight={600} variant="subtitle1" sx={{ display: 'inline' }}>
+            {props.value ? props.value.toLocaleDateString() : 'null'}
+          </WTypography>
+        </WBox>
+      </WCardContent>
+    </WCard>
+  </WGrid>
+)
 
 export const EmptyScreen: FC = () => {
-  const [date, setDate] = useState<Date>(new Date(2017, 8 - 1, 9));
+  const [dateNull, setDateNull] = useState<Date>();
 
   return (
-    <div>
-      <div>
+    <WGrid container spacing={1}>
+      <DateCard title="Value state ile yönetiliyor. İlk değer boş" value={dateNull}>
         <WDatePicker
-          value={date}
-          onChange={(val: any) => setDate(val)}
+          value={dateNull}
+          onChange={(val: any) => setDateNull(val)}
           format="dd.MM.yyyy"
         />
-        {date.toLocaleDateString()}
-      </div>
-    </div>
+        <Alert severity="success" sx={{ mt: 1 }}>
+          Düzgün çalışıyor
+        </Alert>
+      </DateCard>
+    </WGrid>
   );
 }
